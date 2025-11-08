33ed34146ef4c9b80b03025f782acb60688fe634
Langelihle6 <141252621+Langelihle6@users.noreply.github.com>
2025-11-08 22:46:01 +0000

    installed trailbase and changed the URL

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,10 +1,10 @@
-import { Admin, CustomRoutes, ListGuesser } from "react-admin";
+import { Admin, CustomRoutes, ListGuesser, Resource } from "react-admin";
 import { Route } from "react-router-dom";
 import { createTrailbaseProvider } from "./ra-trailbase";
-import { CreateUser, LoginPage } from "./create_user";
+import { CreateUser, LoginPage  } from "./create_user"; 
 
 
-const TRAILBASE_URL = "http://supreme-fishstick-5gqxvgvxwgvvfvrgv-4000.app.github.dev/";
+const TRAILBASE_URL = "http://sturdy-cod-979j676jvvqjf9x7-4000.app.github.dev/";
 const { dataProvider, authProvider } = await createTrailbaseProvider(
   TRAILBASE_URL,
 );
@@ -12,7 +12,7 @@ const { dataProvider, authProvider } = await createTrailbaseProvider(
 function App() {
   return (
     <Admin loginPage={LoginPage} dataProvider={dataProvider} authProvider={authProvider}>
-      <Resource name= "assignments" list= {ListGuesser} />
+      <Resource name= "gear" list= {ListGuesser} />
       <CustomRoutes noLayout>
         <Route path="/create-user" element={<CreateUser />} />
       </CustomRoutes>
